8e8a496eee7bdd2e3e7beabefc48536998e6cec1
Michael Bolton <msbolton@protonmail.com>
2023-08-31 20:23:53 -0400

    Setup api routes, schema models

--- server/index.js
+++ server/index.js
@@ -1,29 +1,40 @@
-import express from "express"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
-import helmet from "helmet"
-import morgan from "morgan"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
-dotenv.config()
-const app = express()
+/** CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended:false }))
-app.use(cors())
+/** ROUTES */
+app.use("/kpi", kpiRoutes);
 
+/** MONGOOSE */
 const PORT = process.env.PORT || 3000;
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(async () => {
-        app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
-    })
-    .catch(error => console.warn(`${error} could not connect`))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+    /** Seed data as needed */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+  })
+  .catch((error) => console.warn(`${error} could not connect`));
